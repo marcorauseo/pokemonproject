dc55a677e2fe41ace2c4f4ce8ee0293cad29ad03
Marco Rauseo <rauseom@libero.it>
2025-10-04 10:24:49 +0200

    sonarlint: removed unused\redundant methods

--- src/main/java/com/example/pokedex/util/ErrorResponse.java
+++ src/main/java/com/example/pokedex/util/ErrorResponse.java
@@ -1,6 +1,4 @@
 package com.example.pokedex.util;
 
 public record ErrorResponse(int status, String message) {
-    public int status() { return status; }
-    public String message() { return message; }
 }
